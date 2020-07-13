595fe86c71bf9a186429f97778eaa93a1980a37f
Jovan Kojadinovic <jkojadinovic@inovatec.ca>
2020-07-13 14:44:38 +0200

    Salesforce data pulled

---
+++ .sfdx/typings/lwc/contentassets.d.ts
@@ -0,0 +1,28 @@
+declare module "@salesforce/contentAssetUrl/X031axe" {
+    var X031axe: string;
+    export default X031axe;
+}
+declare module "@salesforce/contentAssetUrl/X031settings" {
+    var X031settings: string;
+    export default X031settings;
+}
+declare module "@salesforce/contentAssetUrl/X20898" {
+    var X20898: string;
+    export default X20898;
+}
+declare module "@salesforce/contentAssetUrl/blackcalculatornearballpointpenonwh" {
+    var blackcalculatornearballpointpenonwh: string;
+    export default blackcalculatornearballpointpenonwh;
+}
+declare module "@salesforce/contentAssetUrl/coffee_cup_wooden_surface_106291_19" {
+    var coffee_cup_wooden_surface_106291_19: string;
+    export default coffee_cup_wooden_surface_106291_19;
+}
+declare module "@salesforce/contentAssetUrl/rolled20usdollarbill164527" {
+    var rolled20usdollarbill164527: string;
+    export default rolled20usdollarbill164527;
+}
+declare module "@salesforce/contentAssetUrl/roundsilverandgoldcoins730564" {
+    var roundsilverandgoldcoins730564: string;
+    export default roundsilverandgoldcoins730564;
+}
